49f2aa76ce4d29e127113c2abe267cf0a7f8109d
Romain FREYDIGER <romain.freydiger@outlook.fr>
2025-07-30 21:19:18 +0200

    apply changes from updated documentation

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,13 @@
 import 'zone.js';
+
 import { applicationConfig, type Preview } from '@analogjs/storybook-angular';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
+
+// compodoc configuration
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import docJson from '../documentation.json';
+
+setCompodocJson(docJson);
 
 const preview: Preview = {
   decorators: [
